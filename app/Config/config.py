34922ae4ba0054d8ed037e358cd65505f2e8d150
nivesh1000 <nivesh.nk1001@gmail.com>
2025-02-20 15:19:24 +0530

    modified config.py

--- app/Config/config.py
+++ app/Config/config.py
@@ -6,8 +6,6 @@ load_dotenv()
 
 # Credentials
 CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 TENANT_ID = os.getenv("TENANT_ID")
-MAILBOX_USER_ID = os.getenv("MAILBOX_USER_ID")
 EMAIL_API_BASE_URL = os.getenv("EMAIL_API_BASE_URL", "https://graph.microsoft.com/v1.0")
 SCOPES = os.getenv("SCOPES")
